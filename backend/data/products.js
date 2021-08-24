48b11a1f5c017cfc4f0bac10df53719759299baf
Mike Marino <marino1820@gmail.com>
2021-08-24 18:09:54 -0400

    Finished frontend - backend start, express/axios running

--- backend/data/products.js
+++ backend/data/products.js
@@ -34,7 +34,7 @@ const products = [
     brand: 'Cannon',
     category: 'Electronics',
     price: 929.99,
-    countInStock: 5,
+    countInStock: 0,
     rating: 3,
     numReviews: 12,
   },
@@ -79,4 +79,4 @@ const products = [
   },
 ]
 
-export default products
+module.exports = products
