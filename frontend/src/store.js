9d0d19b20e533820fca4afd25abb8f9d5aa3e9c1
Mike Marino <marino1820@gmail.com>
2021-08-29 22:34:31 -0400

    User Authentication with tokens

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,8 @@
 // Store.js Connects reducers and middleware
 
 // createsStore: function to create store
-// combineReducers: Each reducer handles a certain piece of functionality. Such as fetching data from backend, a reducer will handle request, response, and errors 
-// applyMiddleware:  applies middleware 
+// combineReducers: Each reducer handles a certain piece of functionality. Such as fetching data from backend, a reducer will handle request, response, and errors
+// applyMiddleware:  applies middleware
 
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
@@ -12,26 +12,34 @@ import {
 	productDetailsReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
 
 // sets 'productList' as piece of state
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
 	cart: cartReducer,
+	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer,
+	userDetails: userDetailsReducer,
+	userUpdateProfile: userUpdateProfileReducer,
+
 })
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+	? JSON.parse(localStorage.getItem('cartItems'))
+	: []
+
+	const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 // set initial state to have data pre-loaded to state
 const initialState = {
-	cart:{ cartItems: cartItemsFromStorage}
-
+	cart: { cartItems: cartItemsFromStorage },
+	userLogin: { userInfo: userInfoFromStorage },
 }
 
 // array of middleware to be passed to store
 const middleware = [thunk]
-
 
 // create store - pass in reducer (above)
 // composeWithDevTools - enables chrome redux dev tool
